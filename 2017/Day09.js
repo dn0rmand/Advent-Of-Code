b2e16376242f5cdc5969318855c8b7c8d7af28a4
Dominique NORMAND <dominique@normand.us>
2017-12-09 12:39:35 -0500

    Day 9 done ... parsing was tricky

--- 2017/Day09.js
+++ 2017/Day09.js
@@ -7,23 +7,78 @@ module.exports = function()
         input: fs.createReadStream('Data/Day09.data')
     });
 
-    var characters = 0;
-    var lines      = 0;
-
     readInput
     .on('line', (line) => { 
-        characters += line.length;
-        lines++;
-
         processLine(line);
     })
     .on('close', () => {
-        console.log(characters + ' characters in ' + lines + " lines");
         process.exit(0);
     });
 
     function processLine(line)
     {
-        console.log(line);
+        let garbageCount = 0;
+        let count = 0;
+        let index = 0;
+        let length= line.length;
+
+        function processGarbage()
+        {
+            if (index < length && line[index] == '<')
+            {
+                index++;
+                while (index < length)
+                {
+                    let c = line[index++];
+                    if (c == '!')
+                        index++; // Skip next character
+                    else if (c == '>') // end of garbage
+                        break;
+                    else
+                        garbageCount++;
+                }
+            }
+        }
+
+        function processGroup(score)
+        {
+            processGarbage();
+
+            let count = 0;
+
+            if (index < length && line[index] == '{')
+            {
+                index++;
+                
+                count = score + processGroups(score+1);
+
+                if (index >= length || line[index] != '}')
+                    throw "Invalid input";
+
+                index++;
+            }
+
+            processGarbage();
+
+            return count;
+        }
+
+        function processGroups(score)
+        {
+            let groupCount = processGroup(score);
+            
+            while (index < length && line[index] == ',')
+            {
+                index++;
+                groupCount += processGroup(score);
+            }
+
+            return groupCount;
+        }
+
+        let lineValue = processGroups(1);
+
+        console.log("Part 1: " + lineValue + " ( 11089 )");
+        console.log("Part 2: " + garbageCount + " ( 5288 )");
     }
 }
