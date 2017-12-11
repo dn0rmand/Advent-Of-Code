116a9f3a9ae3e617e066a22c65ae75d140ea3aa3
Dominique NORMAND <dominique@normand.us>
2017-12-11 08:46:24 -0500

    Day 11 ... too easy

--- 2017/Day11.js
+++ 2017/Day11.js
@@ -1,29 +1,51 @@
-module.exports = function()
+// module.exports =
+(function()
 {
+    //#region FWK - Read file and load required modules
     const fs = require('fs');
     const readline = require('readline');
+    const parser = require('../tools/parser.js');
 
     const readInput = readline.createInterface({
         input: fs.createReadStream('Data/Day11.data')
     });
 
-    var characters = 0;
-    var lines      = 0;
+    readInput.on('line', (line) => processLine(line)).on('close', () => { dumpResult(); process.exit(0); });
+    //#endregion
 
-    readInput
-    .on('line', (line) => { 
-        characters += line.length;
-        lines++;
+    let distance = 0, maxDistance = 0;
 
-        processLine(line);
-    })
-    .on('close', () => {
-        console.log(characters + ' characters in ' + lines + " lines");
-        process.exit(0);
-    });
+    function dumpResult()
+    {
+        console.log("Distance = " + distance);
+        console.log("Max Distance is " + maxDistance);
+    }
 
     function processLine(line)
     {
-        console.log(line);
+        let x = 0, y = 0;
+
+        line.split(',').forEach(s => {
+            switch(s)
+            {
+                case 'n': y++; break;
+                case 's': y--; break; 
+                case 'w': x--; break;
+                case 'e': x++; break;
+
+                case 'ne': y++; x++; break;
+                case 'nw': y++; x--; break;
+                case 'se': y--; x++; break;
+                case 'sw': y--; x--; break;
+
+                default:
+                    console.log('direction not supported');
+                    break;
+            }
+
+            maxDistance = Math.max(maxDistance, Math.abs(x), Math.abs(y));
+        });
+
+        distance = Math.max( Math.abs(x), Math.abs(y));
     }
-}
+})();
