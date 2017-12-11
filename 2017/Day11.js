2dc9893c806e432da21ff0e02acda179ed9fbcca
Dominique NORMAND <dominique@normand.us>
2017-12-11 18:25:10 -0500

    Use cube coordinates

--- 2017/Day11.js
+++ 2017/Day11.js
@@ -4,7 +4,6 @@
     //#region FWK - Read file and load required modules
     const fs = require('fs');
     const readline = require('readline');
-    const parser = require('../tools/parser.js');
 
     const readInput = readline.createInterface({
         input: fs.createReadStream('Data/Day11.data')
@@ -13,39 +12,56 @@
     readInput.on('line', (line) => processLine(line)).on('close', () => { dumpResult(); process.exit(0); });
     //#endregion
 
-    let distance = 0, maxDistance = 0;
-
     function dumpResult()
     {
-        console.log("Distance = " + distance);
-        console.log("Max Distance is " + maxDistance);
     }
 
     function processLine(line)
     {
-        let x = 0, y = 0;
+        let position = {
+            x: 0, 
+            y: 0, 
+            z: 0
+        };
 
-        line.split(',').forEach(s => {
+        function move(x, y, z)
+        {
+            position.x += x;
+            position.y += y;
+            position.z += z;
+        }
+
+        function getDistance()
+        {
+            let a = position;
+            let b = { x: 0, y:0, z: 0};
+
+            return Math.max(Math.abs(a.x - b.x), Math.abs(a.y - b.y), Math.abs(a.z - b.z));
+        }
+
+        let distance = 0, maxDistance = 0;
+                
+        line.split(',').forEach(s => 
+        {
             switch(s)
             {
-                case 'n': y++; break;
-                case 's': y--; break; 
-                case 'w': x--; break;
-                case 'e': x++; break;
-
-                case 'ne': y++; x++; break;
-                case 'nw': y++; x--; break;
-                case 'se': y--; x++; break;
-                case 'sw': y--; x--; break;
+                case 'n' : move(  0,  1, -1); break;
+                case 's' : move(  0, -1,  1); break;
+                case 'ne': move(  1,  0, -1); break;
+                case 'nw': move( -1,  1,  0); break;
+                case 'se': move(  1, -1,  0); break;
+                case 'sw': move( -1,  0,  1); break;
 
                 default:
                     console.log('direction not supported');
                     break;
             }
 
-            maxDistance = Math.max(maxDistance, Math.abs(x), Math.abs(y));
+            maxDistance = Math.max(maxDistance, getDistance());
         });
 
-        distance = Math.max( Math.abs(x), Math.abs(y));
+        distance = getDistance();
+
+        console.log('Distance = ' + distance + ' - Max is ' + maxDistance);
     }
 })();
