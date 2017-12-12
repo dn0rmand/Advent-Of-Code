544b0cc19cebd55dad454176a348028f97b577c7
Dominique NORMAND <dominique@normand.us>
2017-12-12 07:22:19 -0500

    Day 12 done

--- 2017/Day12.js
+++ 2017/Day12.js
@@ -1,5 +1,4 @@
-//module.exports = 
-(function()
+module.exports = function()
 {
     //#region FWK - Read file and load required modules
     const fs = require('fs');
@@ -14,9 +13,71 @@
 
     function dumpResult()
     {
+        let destin = { '0':1 };
+
+        let program = getProgram(0);
+        
+        addDestinations(destin, program);
+
+        console.log('Part 1: ' + Object.keys(destin).length + " program connected to 0");
+
+        let groupCount = 1;
+        Object.keys(programs).forEach( p => {
+            if (processed[p] === undefined)
+            {
+                destin = {}; destin[p] = 1;
+                processed[p] = 1;
+                groupCount++;
+                addDestinations(destin, getProgram(p));
+            }
+        });
+
+        console.log('Part 2: ' + groupCount + " groups of programs");
+    }
+
+    let programs  = {};
+    let processed = {};
+
+    function addDestinations(destin, program)
+    {
+        Object.keys(program).forEach( d => {
+            if (destin[d] === undefined)
+            {
+                destin[d] = 1;
+                var p = getProgram(d);
+                addDestinations(destin, p);
+            }
+            if (processed[d] === undefined)
+                processed[d] = 1;
+        })
+    }
+
+    function getProgram(number)
+    {
+        let program = programs[number];
+        if (program === undefined)
+        {
+            program = { };
+            programs[number] = program;
+        }
+        return program;
     }
 
     function processLine(line)
     {
+        let parse = new parser(line);
+        let program = getProgram(parse.getNumber(true));
+
+        parse.expectOperator('<');
+        parse.expectOperator('-');
+        parse.expectOperator('>');
+        let destin = parse.getNumber(true);
+        program[destin] = 1;
+        while(! parse.endOfLine())
+        {            
+            parse.expectOperator(',');
+            destin = parse.getNumber(true);
+            program[destin] = 1;
+        }
     }
-})();
+}
