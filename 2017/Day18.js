9b006fcb10ffb8dd00f91bd6640dd18e22766be0
Dominique NORMAND <dominique@normand.us>
2017-12-18 10:47:01 -0500

    Extracted this year's language for future reuse

--- 2017/Day18.js
+++ 2017/Day18.js
@@ -1,120 +1,10 @@
 module.exports = async function()
 {
-    //#region FWK - Read file and load required modules
-    const fs = require('fs');
-    const readline = require('readline');
-    const parser = require('../tools/parser.js');
-    const compiler = require('../tools/interpreter.js');
+    const compiler = require('../tools/duet.js');
 
-    let queue = [ [] , [] ];
-
-    async function createVM(pid, part1)
+    async function create(pid, sndQueue, rcvQueue)
     {
-        let vm = new compiler();
-        vm.sent = 0;
-        
-        queue[pid] = []; // reset for part 2
-
-        function convert(arg1)
-        {
-            if (typeof(arg1) === "string")
-                return vm.$registers[arg1];
-            else 
-                return arg1;
-        }
-
-        vm.add(
-            'snd',
-            arg1 => {
-                let v = convert(arg1);
-                queue[pid].push(v);
-                vm.sent++;
-            },
-            (instruction, parser) => {
-                instruction.arg1 = vm.createArgument(parser.getValue());
-            }
-        );
-        
-        vm.add(
-            'set',
-            (arg1, arg2) => {
-                vm.$registers[arg1] = convert(arg2);
-            },
-            (instruction, parser) => {
-                instruction.arg1 = parser.getToken();
-                instruction.arg2 = vm.createArgument(parser.getValue());
-            }
-        );
-        
-        vm.add(
-            'add',
-            (arg1, arg2) => {
-                vm.$registers[arg1] += convert(arg2);
-            },
-            (instruction, parser) => {
-                instruction.arg1 = parser.getToken();
-                instruction.arg2 = vm.createArgument(parser.getValue());
-            }
-        );
-        
-        vm.add(
-            'mul',
-            (arg1, arg2) => {
-                vm.$registers[arg1] *= convert(arg2);
-            },
-            (instruction, parser) => {
-                instruction.arg1 = parser.getToken();
-                instruction.arg2 = vm.createArgument(parser.getValue());
-            }
-        );
-        
-        vm.add(
-            'mod',
-            (arg1, arg2) => {
-                vm.$registers[arg1] = vm.$registers[arg1] % convert(arg2);
-            },
-            (instruction, parser) => {
-                instruction.arg1 = parser.getToken();
-                instruction.arg2 = vm.createArgument(parser.getValue());
-            }
-        );
-        
-        vm.add(
-            'rcv',
-            (arg1) => {
-                if (part1 === true) {
-                    console.log("Part 1: Last sound is " + queue[pid].pop());
-                    return 1000; // move out to stop program
-                }
-                else
-                {
-                    var q = queue[(pid+1) % 2]; // other program's queue
-                    if (q.length > 0)
-                    {
-                        vm.$registers[arg1] = q.shift();
-                    }
-                    else
-                        return 0; // cause the program to stay on this location
-                }
-            },
-            (instruction, parser) => {
-                instruction.arg1 = parser.getToken();
-            }
-        );
-        
-        vm.add(
-            'jgz',
-            (arg1, arg2) => {
-                if (convert(arg1) > 0)
-                {
-                    return convert(arg2);
-                }
-            },
-            (instruction, parser) => {
-                instruction.arg1 = vm.createArgument(parser.getValue());
-                instruction.arg2 = vm.createArgument(parser.getValue());
-            }
-        );
+        let vm = compiler.create(pid, sndQueue, rcvQueue);
 
         await vm.parse("Data/Day18.data");
 
@@ -125,38 +15,33 @@ module.exports = async function()
 
     async function solve1()
     {
-        let vm = await createVM(0, true);
+        let queue = [];
+        let vm = await create(0, queue, queue);
+
+        vm.didReceive = function(value)
+        {
+            if (queue.length > 0)
+                value = queue.pop(); // We want the last value not the first one!!!!
+            console.log('Part 1: ' + value + ' (4601)');
+            vm.$current = 10000; // Move out to stop program
+        };
 
         vm.execute();
     }    
 
     async function solve2()
     {
-        function isStopped(vm)
-        {
-            if (vm.$current >= vm.$instructions.length)
-                return true;
-        }
+        let queue0 = [];
+        let queue1 = [];
 
-        let vm0 = await createVM(0);
-        let vm1 = await createVM(1);
+        let vms = [
+            await create(0, queue0, queue1),
+            await create(1, queue1, queue0)
+        ];
 
-        while (true)
-        {
-            let step0 = vm0.$current;
-            let step1 = vm1.$current;
+        compiler.run(vms);
 
-            vm0.runStep();
-            vm1.runStep();
-
-            if (isStopped(vm0) || isStopped(vm1))
-                break;
-            if (step0 === vm0.$current && step1 === vm1.$current) // dead lock
-                break;
-        }
-        
-        console.log("Program 0 sent " + vm0.sent + " times ");
-        console.log("Program 1 sent " + vm1.sent + " times ");
+        console.log("Program 1 sent " + vms[1].sent + " times (6858)");
     }
 
     await solve1();
---
+++ tools/duet.js
@@ -0,0 +1,132 @@
+module.exports.create = function(pid, sndQueue, rcvQueue)
+{
+    const compiler = require('../tools/interpreter.js');
+
+    let vm = new compiler();
+
+    vm.sent = 0;
+        
+    function convert(arg1)
+    {
+        if (typeof(arg1) === "string")
+            return vm.$registers[arg1];
+        else 
+            return arg1;
+    }
+
+    vm.add(
+        'snd',
+        arg1 => {
+            let v = convert(arg1);
+            sndQueue.push(v);
+            vm.sent++;
+        },
+        (instruction, parser) => {
+            instruction.arg1 = vm.createArgument(parser.getValue());
+        }
+    );
+    
+    vm.add(
+        'set',
+        (arg1, arg2) => {
+            vm.$registers[arg1] = convert(arg2);
+        },
+        (instruction, parser) => {
+            instruction.arg1 = parser.getToken();
+            instruction.arg2 = vm.createArgument(parser.getValue());
+        }
+    );
+    
+    vm.add(
+        'add',
+        (arg1, arg2) => {
+            vm.$registers[arg1] += convert(arg2);
+        },
+        (instruction, parser) => {
+            instruction.arg1 = parser.getToken();
+            instruction.arg2 = vm.createArgument(parser.getValue());
+        }
+    );
+    
+    vm.add(
+        'mul',
+        (arg1, arg2) => {
+            vm.$registers[arg1] *= convert(arg2);
+        },
+        (instruction, parser) => {
+            instruction.arg1 = parser.getToken();
+            instruction.arg2 = vm.createArgument(parser.getValue());
+        }
+    );
+    
+    vm.add(
+        'mod',
+        (arg1, arg2) => {
+            vm.$registers[arg1] = vm.$registers[arg1] % convert(arg2);
+        },
+        (instruction, parser) => {
+            instruction.arg1 = parser.getToken();
+            instruction.arg2 = vm.createArgument(parser.getValue());
+        }
+    );
+    
+    vm.add(
+        'rcv',
+        (arg1) => {
+            if (rcvQueue.length > 0)
+            {
+                let v = rcvQueue.shift();
+                vm.$registers[arg1] = v;
+                vm.didReceive(v);
+            }
+            else
+                return 0; // cause the program to stay on this location
+        },
+        (instruction, parser) => {
+            instruction.arg1 = parser.getToken();
+        }
+    );
+    
+    vm.add(
+        'jgz',
+        (arg1, arg2) => {
+            if (convert(arg1) > 0)
+            {
+                return convert(arg2);
+            }
+        },
+        (instruction, parser) => {
+            instruction.arg1 = vm.createArgument(parser.getValue());
+            instruction.arg2 = vm.createArgument(parser.getValue());
+        }
+    );
+
+    vm.didReceive = function(value) {
+        // does nothing by default
+    }
+    
+    return vm;
+};
+
+module.exports.run = function(vms)
+{
+    let moved = true;
+    while (moved)
+    {
+        moved = false;
+
+        for(let i = 0; i < vms.length; i++)
+        {
+            let vm = vms[i];
+            if (vm.stopped)
+                continue;
+
+            let step = vm.$current;
+            vm.runStep();
+            if (vm.$current !== step)
+                moved = true;
+            if (vm.$current >= vm.$instructions.length)
+                vm.stopped = true;
+        }
+    }    
+}
