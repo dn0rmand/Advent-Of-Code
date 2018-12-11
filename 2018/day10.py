acb237cb83d1a77d2c2d5654adab5a0975adc350
Dominique NORMAND <dominique@normand.us>
2018-12-10 23:23:40 -0500

    some changes

--- 2018/day10.py
+++ 2018/day10.py
@@ -1,47 +1,42 @@
 from collections import defaultdict
+from collections import namedtuple
 
-X = 1
-Y = 0
-VX= 2
-VY= 3
+Point = namedtuple("Point", "x, y, vx, vy")
 
 def loadData():
     points = []
+
     for line in open('2018/data/day10.data', 'rt'):
-        x = line[10:16]
-        x = int(x)
-        y = line[18:24]
-        y = int(y)
-        vx= line[36:38]
-        vx= int(vx)
-        vy= line[40:42]
-        vy= int(vy)
-        points.append([y, x, vx, vy])
+        x = int(line[10:16])
+        y = int(line[18:24])
+        vx= int(line[36:38])
+        vy= int(line[40:42])
+        points.append(Point(x=x, y=y, vx=vx, vy=vy))
     return points
 
-def move(points):
+def calculate(points, time):
     minX, maxX, minY, maxY = 99999,-99999,99999,-99999 
 
     for point in points:
-        point[X] += point[VX]
-        point[Y] += point[VY]
-        if point[X] < minX: minX = point[X]
-        if point[X] > maxX: maxX = point[X]
-        if point[Y] < minY: minY = point[Y]
-        if point[Y] > maxY: maxY = point[Y]
+        x = point.x + time * point.vx
+        y = point.y + time * point.vy
+
+        minX = min(x, minX)
+        minY = min(y, minY)
+        maxX = max(x, maxX)
+        maxY = max(y, maxY)
 
     w = maxX - minX
     h = maxY - minY
 
-    if h >= 10:
-        return False, minX, minY, w, h
-    else:
-        return True, minX, minY, w, h
+    return minX, minY, w, h
 
-def dump(points, x, y, w, h):
+def dump(points, time):
+    x, y, w, h = calculate(points, time)
+
     print('')
     for yy in range(y, y+h+1):
-        pts = (p[X]-x for p in points if p[Y] == yy)
+        pts = (p.x + time * p.vx - x for p in points if p.y + time * p.vy == yy)
         line = [' ']*(w+1)
         for xx in pts:
             line[xx] = '#'
@@ -49,14 +44,19 @@ def dump(points, x, y, w, h):
     print('')
 
 def solve(points):
-    time = 0
-    d = False
-    while not d:
-        d, x, y, w, h = move(points)
+    time = 10000
+    _, _, _, h = calculate(points, time)
+
+    previous = h
+    while True:
+        _, _, _, h = calculate(points, time+1)
+        if h > previous:
+            break
+        previous = h
         time += 1
 
     print("Answer to Part 1 is:")
-    dump(points, x, y, w, h)
+    dump(points, time)
     print("Answer to part 2 is", time)
 
 print("")
