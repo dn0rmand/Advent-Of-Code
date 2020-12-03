ab1823f342292acdf1aaa21fe88c9c3e0a78a0c8
Dominique NORMAND <dominique@normand.us>
2020-12-03 08:09:11 -0500

    day3 done

---
+++ 2020/day3.js
@@ -0,0 +1,90 @@
+const assert = require('assert');
+
+function loadTestData()
+{
+    return [
+        '..##.......',
+        '#...#...#..',
+        '.#....#..#.',
+        '..#.#...#.#',
+        '.#...##..#.',
+        '..#.##.....',
+        '.#.#.#....#',
+        '.#........#',
+        '#.##...#...',
+        '#...##....#',
+        '.#..#...#.#',
+    ];
+}
+
+function loadData()
+{
+    const readFile = require("advent_tools/readfile");
+
+    const entries = [];
+
+    for(const line of readFile(__filename))
+    {
+        entries.push(line);
+    }
+
+    return entries;
+}
+
+function calculateTrees(input, {  right, down })
+{
+    let x = 0, y = 0;
+    const width = input[0].length;
+
+    assert.strictEqual(input[y][x], '.');
+
+    let trees = 0;
+    while (y < input.length) {
+        x = (x + right) % width;
+        y = y + down;
+
+        if (y < input.length && input[y][x] === '#') {
+            trees++;
+        }
+    }
+
+    return trees;
+}
+
+function part1()
+{
+    const slope = {right: 3, down: 1};
+
+    const testInput = loadTestData();
+    assert.strictEqual(calculateTrees(testInput, slope), 7);
+
+    const input = loadData();
+    const trees = calculateTrees(input, slope);
+    
+    return trees;
+}
+
+function part2()
+{
+    const slopes = [
+        {right: 1, down: 1},
+        {right: 3, down: 1},
+        {right: 5, down: 1},
+        {right: 7, down: 1},
+        {right: 1, down: 2},
+    ];
+
+    const testInput = loadTestData();
+
+    const testTrees = slopes.reduce((a, slope) => a * calculateTrees(testInput, slope), 1);
+    assert.strictEqual(testTrees, 336);
+
+    const input = loadData();
+    const trees = slopes.reduce((a, slope) => a * calculateTrees(input, slope), 1);
+    
+    return trees;
+}
+
+console.log('--- Advent of Code day 3 ---');
+console.log(`Part 1: ${part1()}`);
+console.log(`Part 2: ${part2()}`);
