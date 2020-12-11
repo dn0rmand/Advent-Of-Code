c2bfaf5014f00a57a3ffb2b76b89a02fce153ce1
Dominique Normand <dnormand@us.imshealth.com>
2020-12-10 19:13:24 -0500

    faster , better + alternate solution

--- 2020/day10.js
+++ 2020/day10.js
@@ -14,78 +14,64 @@ function loadData()
     return entries;
 }
 
-function part1()
+function part1(input)
 {
-    const input = loadData();
-
     let j3 = 1;
     let j1 = 0;
     let j  = 0;
 
     for(const a of input) {
-        switch (a-j) {
-            case 0:
-                throw "ERROR";
-            case 1:
-                j1++;
-                break;
-            case 2:
-                break;
-            case 3:
-                j3++;
-                break;
-            default:
-                throw "ERROR";
-        }
+        const o = a-j;
+        if (o === 1)
+            j1++;
+        else if (o === 3)
+            j3++;
         j = a;
     }
 
     return j1*j3;
 }
 
-function part2()
+function part2(input)
 {
-    const makeKey = state => `${state.jolt}-${state.input.join(':')}`;
+    const target  = input[input.length-1]+3;
 
-    let states = new Map();
-    let mainInput = loadData().reverse();
-
-    states.set('start', { jolt:mainInput[0]+3, input: mainInput, count: 1 });
+    let states = [{ jolt: 0, index: 0, count: 1 }];
 
     let total = 0;
 
-    while(states.size > 0) {
-        const newStates = new Map();
+    while(states.length > 0)
+    {
+        const newStates = [];
 
-        for(const state of states.values()) 
+        for(const idx in states)
         {
-            if (state.jolt <= 3) 
+            const state = states[idx];
+
+            if (state.jolt+3 === target)
             {
-                // reached a starting point
                 total += state.count;
             }
 
-            for(let i = 0; i < state.input.length; i++) 
+            for(let i = 0; i < 4; i++)
             {
-                const a = state.input[i];
-                if ((state.jolt - a) > 3)
+                const a = input[i + state.index];
+
+                if (!a || (a - state.jolt) > 3)
                     break;
 
-                const newState = {
-                    jolt: a,
-                    input: state.input.slice(i+1),
-                    count: state.count,
-                };
-
-                const key = makeKey(newState);
-                const old = newStates.get(key);
-                if (old) 
+                const old = newStates[a];
+                if (old)
                 {
-                    old.count += newState.count;
-                } 
-                else 
+                    old.count += state.count;
+                }
+                else
                 {
-                    newStates.set(key, newState);
+                    newStates[a] = {
+                        jolt: a,
+                        index: state.index + i + 1,
+                        count: state.count,
+                    };
                 }
             }
         }
@@ -96,11 +82,42 @@ function part2()
     return total;
 }
 
+function part2B(input)
+{
+    const target = input[input.length-1] + 3;
+    const counts = new Array(target+1);
+
+    counts.fill(0);
+    counts[0] = 1;
+
+    input.push(target);
+
+    for(let i = 0; i < input.length; i++) {
+        const value = input[i];
+        for(let a = 1; a <= 3; a++) {
+            const idx = value-a;
+            if (idx >= 0)
+                counts[value] += counts[idx];
+        }
+    }
+
+    const answer = counts[target];
+
+    return answer;
+}
+
 console.log('--- Advent of Code day 10 ---');
 
-console.time('Day10');
+const input = loadData();
 
-console.log(`Part 1: ${part1()}`);
-console.log(`Part 2: ${part2()}`);
+console.time('Day10-1');
+console.log(`Part 1: ${part1(input)}`);
+console.timeLog('Day10-1', 'to execute part 2 of day 10');
 
-console.timeLog('Day10', 'to execute both parts of day 10');
+console.time('Day10-2');
+console.log(`Part 2: ${part2(input)}`);
+console.timeLog('Day10-2', 'to execute part 2 of day 10');
+
+console.time('Day10-2b');
+console.log(`Part 2: ${part2B(input)}`);
+console.timeLog('Day10-2b', 'to execute part 2 (B) of day 10');
